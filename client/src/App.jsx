484e742c0add5878a85f5c6d04c731a12adb08f0
dibkb <dibas9110@gmail.com>
2023-02-28 11:03:39 +0530

    global email

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import "./App.css";
 import Home from "./Home";
 import ChatSection from "./ChatSection";
 // const socket = socketIO.connect("http://localhost:8080");
+// adding global username
 function App() {
   return (
     <Routes>
